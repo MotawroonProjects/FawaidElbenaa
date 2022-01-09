ddf63c9754293d7339f5a253cc93cbba3e80a6c6
filesdev7 <filesdev7@gmail.com>
2022-01-09 11:59:08 +0200

    emad

--- app/src/main/java/com/fawaid_elbenaa/tags/Tags.java
+++ app/src/main/java/com/fawaid_elbenaa/tags/Tags.java
@@ -2,12 +2,12 @@ package com.fawaid_elbenaa.tags;
 
 public class Tags {
 
-    public static String base_url = "http://www.obibusiness.com/";
+    public static String base_url = "http://faweeth.com/";
     public static final String IMAGE_URL= base_url+"storage/uploads/";
     public static final String IMAGE_Places_URL = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=";
     public static final String session_login = "login";
     public static final String session_logout = "logout";
-    public static final String not_tag = "obi_not_tag";
+    public static final String not_tag = "fawaed_not_tag";
     public static final int not_id = 3254;
 
    /*
